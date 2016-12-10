252a9f17ce85ace64e8bdcf68186ea47df64f6f0
vinkrish <vinaykrishna1989@gmail.com>
2016-12-10 16:00:42 +0530

    cce-scholastic resource/service added

---
+++ src/main/java/com/project/guldu/model/CceCoscholastic.java
@@ -0,0 +1,27 @@
+package com.project.guldu.model;
+
+public class CceCoscholastic {
+	private long id;
+	private long schoolId;
+	private String name;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public long getSchoolId() {
+		return schoolId;
+	}
+	public void setSchoolId(long schoolId) {
+		this.schoolId = schoolId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ src/main/java/com/project/guldu/resources/CceCoscholasticResource.java
@@ -0,0 +1,54 @@
+package com.project.guldu.resources;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.project.guldu.model.CceCoscholastic;
+import com.project.guldu.service.CceCoscholasticService;
+
+import authentication.Secured;
+
+@Path("/ccecoscholastic")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class CceCoscholasticResource {
+	
+	CceCoscholasticService service = new CceCoscholasticService();
+
+	@Secured
+	@GET
+	@Path("school/{schoolId}")
+	public List<CceCoscholastic> getCceCoscholastics(@PathParam("schoolId") long schoolId) {
+		return service.getCceCoscholastics(schoolId);
+	}
+
+	@Secured
+	@POST
+	public CceCoscholastic add(CceCoscholastic CceCoSch) {
+		return service.add(CceCoSch);
+	}
+	
+	@Secured
+	@PUT
+	@Path("/{classId}")
+	public void update(CceCoscholastic CceCoSch) {
+		service.update(CceCoSch);
+	}
+	
+	@Secured
+	@DELETE
+	@Path("/{classId}")
+	public void delete(@PathParam("classId") long classId) {
+		service.delete(classId);
+	}
+
+}
